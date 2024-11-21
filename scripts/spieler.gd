03d0cf57189cad7d57d3c24b0d343606b45825ba
Felix <fhillma@web.de>
2024-11-21 16:23:13 +0100

    letzter stand: Wasser drin und dino noch als payler + dino kann nach oben fliegen

--- scripts/spieler.gd
+++ scripts/spieler.gd
@@ -2,16 +2,16 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -500.0
+const JUMP_VELOCITY = -200.0
 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	#if not is_on_floor():
+		#velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_pressed("ui_accept"):
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.

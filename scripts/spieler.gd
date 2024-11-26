b533d7ad0a0ff3e617a6fb1389f31e8432366ec7
Felix-Hill <fhillma@web.de>
2024-11-26 14:14:15 +0100

    jetski als char genutzt und fahren macht spaß ( theroretisch reicht mir das mit dem fahren so)

--- scripts/spieler.gd
+++ scripts/spieler.gd
@@ -3,23 +3,37 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -200.0
+@export var speed = 400
+@export var rotation_speed = 2.5
 
+var rotation_direction = 0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	#if not is_on_floor():
 		#velocity += get_gravity() * delta
-
+	get_input()
+	rotation += rotation_direction * rotation_speed * delta
+	
 	# Handle jump.
-	if Input.is_action_pressed("ui_accept"):
-		velocity.y = JUMP_VELOCITY
+	#if Input.is_action_pressed("ui_accept"):
+	#	velocity.y = JUMP_VELOCITY
+	#else:
+	#	velocity.y = velocity.y * 0.99
+		
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	#var direction := Input.get_axis("ui_left", "ui_right")
+	#if direction:
+	#	velocity.x = direction * SPEED
+	#else:
+	#	velocity.x = move_toward(velocity.x, 0, SPEED)
 	move_and_slide()
+	
+func get_input():
+	rotation_direction = Input.get_axis("ui_left", "ui_right")
+	if Input.is_action_pressed("ui_up"):
+		velocity = transform.x *  speed
+	else:
+		velocity = velocity * 0.97

a8e2f351f1b25fc86cb3277a623329ee156f383a
Felix <fhillma@web.de>
2025-01-10 17:50:17 +0100

    fertig

--- scripts/death_message_2.gd
+++ scripts/death_message_2.gd
@@ -1,11 +1,12 @@
 extends Label
 
+var last_score = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	self.text = str(get_parent().get_parent().score)
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	self.text = "score " +str(get_parent().get_parent().score)
+	pass
